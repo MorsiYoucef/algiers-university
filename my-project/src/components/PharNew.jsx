11954db800656ea4851823da30499bc12457d77e
Your Name <141324213+MorsiYoucef@users.noreply.github.com>
2024-05-23 11:15:00 +0100

    new commit

--- my-project/src/components/PharNew.jsx
+++ my-project/src/components/PharNew.jsx
@@ -1,6 +1,6 @@
 const PharNew = ({ title }) => {
   return (
-    <button className=" bg-yellow-500 text-white flex w-full h-full border-b-2 border-l-2 justify-start gap-3  items-center pl-3">
+    <button className=" bg-my-blue text-white flex w-full h-full border-b-2 border-l-2 justify-start gap-3  items-center pl-3">
       <h1 className=" font-bold text-[18px]">{title}</h1>
     </button>
   )
--- my-project/src/components/info.jsx
+++ my-project/src/components/info.jsx
@@ -6,10 +6,147 @@ const Info = () => {
   return (
     <section>
       <div className=" bg-my-green text-white  ">
-        <ul className=" flex flex-row justify-center items-center gap-24 w-[1286px]">
-          {scienceNav.map((sience) => (
-            <li>{sience.title}</li>
-          ))}
+        <ul className=" flex flex-row justify-center items-center gap-20 w-[1286px]">
+          <li>
+            <div class="dropdown">
+              <button className="dropbtn font-bold ">
+                Acueille
+                <i class="fa fa-caret-down"></i>
+              </button>
+              <div class="dropdown-content">
+                <a href="#" className=" font-bold hover:text-my-blue">
+                  Activités du Recteur
+                </a>
+                <a href="#" className=" font-bold hover:text-my-blue">
+                  Calendrier des évènements
+                </a>
+                <a href="#" className=" font-bold hover:text-my-blue">
+                  Consultations et appels d’offres
+                </a>
+                <a href="#" className=" font-bold hover:text-my-blue">
+                  Textes réglementaires
+                </a>
+                <a href="#" className=" font-bold hover:text-my-blue">
+                  consultation et appel d'offre
+                </a>
+                <a href="#" className=" font-bold hover:text-my-blue">
+                  text reglementaire
+                </a>
+              </div>
+            </div>
+          </li>
+          <li>
+            <div class="dropdown">
+              <button className="dropbtn font-bold ">
+                formation
+                <i class="fa fa-caret-down"></i>
+              </button>
+              <div class="dropdown-content">
+                <a href="#" className=" font-bold hover:text-my-blue">
+                  Activités du Recteur
+                </a>
+                <a href="#" className=" font-bold hover:text-my-blue">
+                  Calendrier des évènements
+                </a>
+                <a href="#" className=" font-bold hover:text-my-blue">
+                  Consultations et appels d’offres
+                </a>
+                <a href="#" className=" font-bold hover:text-my-blue">
+                  Textes réglementaires
+                </a>
+                <a href="#" className=" font-bold hover:text-my-blue">
+                  consultation et appel d'offre
+                </a>
+                <a href="#" className=" font-bold hover:text-my-blue">
+                  text reglementaire
+                </a>
+              </div>
+            </div>
+          </li>
+          <li>
+            <div class="dropdown">
+              <button className="dropbtn font-bold ">
+                Departement
+                <i class="fa fa-caret-down"></i>
+              </button>
+              <div class="dropdown-content">
+                <a href="#" className=" font-bold hover:text-my-blue">
+                  Activités du Recteur
+                </a>
+                <a href="#" className=" font-bold hover:text-my-blue">
+                  Calendrier des évènements
+                </a>
+                <a href="#" className=" font-bold hover:text-my-blue">
+                  Consultations et appels d’offres
+                </a>
+                <a href="#" className=" font-bold hover:text-my-blue">
+                  Textes réglementaires
+                </a>
+                <a href="#" className=" font-bold hover:text-my-blue">
+                  consultation et appel d'offre
+                </a>
+                <a href="#" className=" font-bold hover:text-my-blue">
+                  text reglementaire
+                </a>
+              </div>
+            </div>
+          </li>
+          <li>
+            <div class="dropdown">
+              <button className="dropbtn font-bold ">
+                Vie a la faculte
+                <i class="fa fa-caret-down"></i>
+              </button>
+              <div class="dropdown-content">
+                <a href="#" className=" font-bold hover:text-my-blue">
+                  Activités du Recteur
+                </a>
+                <a href="#" className=" font-bold hover:text-my-blue">
+                  Calendrier des évènements
+                </a>
+                <a href="#" className=" font-bold hover:text-my-blue">
+                  Consultations et appels d’offres
+                </a>
+                <a href="#" className=" font-bold hover:text-my-blue">
+                  Textes réglementaires
+                </a>
+                <a href="#" className=" font-bold hover:text-my-blue">
+                  consultation et appel d'offre
+                </a>
+                <a href="#" className=" font-bold hover:text-my-blue">
+                  text reglementaire
+                </a>
+              </div>
+            </div>
+          </li>
+          <li>
+            <div class="dropdown">
+              <button className="dropbtn font-bold ">
+                Bibliotheque
+                <i class="fa fa-caret-down"></i>
+              </button>
+              <div class="dropdown-content">
+                <a href="#" className=" font-bold hover:text-my-blue">
+                  Activités du Recteur
+                </a>
+                <a href="#" className=" font-bold hover:text-my-blue">
+                  Calendrier des évènements
+                </a>
+                <a href="#" className=" font-bold hover:text-my-blue">
+                  Consultations et appels d’offres
+                </a>
+                <a href="#" className=" font-bold hover:text-my-blue">
+                  Textes réglementaires
+                </a>
+                <a href="#" className=" font-bold hover:text-my-blue">
+                  consultation et appel d'offre
+                </a>
+                <a href="#" className=" font-bold hover:text-my-blue">
+                  text reglementaire
+                </a>
+              </div>
+            </div>
+          </li>
           <li>
             <SearchBar />
           </li>
